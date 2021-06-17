3cdc8cd99f2d8513d43c8370505f99f5a7c35c93
Tristal Li <yanlinlily@126.com>
2021-06-17 13:34:57 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import flask
-from flask import Flask
-from flask import request, url_for, redirect, flash, render_template
+from flask import Flask, request, url_for, redirect, flash, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_required, logout_user, UserMixin, login_user, current_user
 import os
 import sys
 import click
@@ -21,31 +22,102 @@ app.config['SECRET_KEY'] = 'dev'
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: {0}'.format(name)
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return "Test Page"
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 # Two data tables
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
+
+# Initialize admin account
+@app.cli.command()
+@click.option("--username", prompt = True, help = "The username used to login")
+@click.option("--password", prompt = True, hide_input = True, confirmation_prompt = True, help = "The password used to login")
+def admin(username, password):
+    '''create user'''
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo("Updating user....")
+        user.name = username
+        user.set_password(password)
+    else:
+        click.echo("Creating user...")
+        user = User(username = username, name = "Admin")
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo("Done.")
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+# login page
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if not username or not password:
+            flash("Invalid input.")
+            return redirect(url_for("login"))
+
+        user = User.query.first()
+        if user.username == username and user.validate_password(password):
+            login_user(user)
+            flash("Login success.")
+            return redirect(url_for("index"))
+
+        flash("Invalid username or password.")
+        return redirect(url_for("login"))
+    return render_template("login.html")
+
+# log out
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Goodbye.")
+    return(redirect(url_for("index")))
+
+# settings
+@app.route("/settings", methods = ["GET", "POST"])
+@login_required
+def settings():
+    if request.method == "POST":
+        name = request.form["name"]
+
+        if not name or len(name) > 20:
+            flash ("Invalid input")
+            return redirect(url_for("settings"))
+
+        current_user.name = name
+        db.session.commit()
+        flash("Settings updated.")
+        return redirect(url_for("index"))
+    return render_template("settings.html")
+
+
+
 
 # initialize database
 @app.cli.command()
@@ -103,6 +175,8 @@ def page_not_found(e):  # 接受异常对象作为参数
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for("login"))
         title = request.form.get('title')
         year = request.form.get('year')
         if not title or not year or len(year) != 4 or len(title) > 60:
@@ -118,7 +192,8 @@ def index():
     return render_template('index.html', movies = movies)
 
 # Editing page
-@app.route("/edit/<int:movie_id>", methods = ["GET", "POST"])
+@app.route("/movie/edit/<int:movie_id>", methods = ["GET", "POST"])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -137,7 +212,8 @@ def edit(movie_id):
     return render_template('edit.html', movie = movie)
 
 # Deleting records
-@app.route("/delete/<int:movie_id>", methods = ["GET", "POST"])
+@app.route("/movie/delete/<int:movie_id>", methods = ["GET", "POST"])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
